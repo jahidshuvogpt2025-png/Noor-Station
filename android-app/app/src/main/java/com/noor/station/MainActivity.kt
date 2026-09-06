9af47b8da370fcaf09d96d2d73ee0d49924dbe04
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-09-06 16:45:35 +0600

    Add Android app theme and main dashboard layout

---
+++ android-app/app/src/main/java/com/noor/station/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.noor.station
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
