27dc906afec557e6a6bbcc6e8796a134084b1d32
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-09-06 16:21:41 +0600

    Add Noor repository base layer

---
+++ core/database/repository/NoorRepository.kt
@@ -0,0 +1,13 @@
+package com.noor.core.database.repository
+
+class NoorRepository {
+
+    fun saveData() {
+        // Future database save operation
+    }
+
+    fun loadData() {
+        // Future database read operation
+    }
+
+}
